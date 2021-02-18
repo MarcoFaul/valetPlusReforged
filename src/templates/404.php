101cc55de456302dc25c380656a1a72779d02bba
marcofaul <marcofaul@protonmail.com>
2021-02-18 18:46:54 +0100

    Remove unused dashboard tab on 404 page

--- src/templates/404.php
+++ src/templates/404.php
@@ -31,21 +31,6 @@
                 </div>
             </div>
         </div>
-        <div class="hidden bg-blue-500 md:block md:bg-white md:border-b">
-            <div class="container mx-auto px-4">
-                <div class="md:flex">
-                    <div class="flex -mb-px mr-8">
-                        <!--                        Dashboard-->
-                        <a href="#" class="no-underline text-white md:text-blue-500 flex items-center py-4 border-b border-blue-500">
-                            <svg class="h-6 w-6 fill-current mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M3.889 3h6.222a.9.9 0 0 1 .889.91v8.18a.9.9 0 0 1-.889.91H3.89A.9.9 0 0 1 3 12.09V3.91A.9.9 0 0 1 3.889 3zM3.889 15h6.222c.491 0 .889.384.889.857v4.286c0 .473-.398.857-.889.857H3.89C3.398 21 3 20.616 3 20.143v-4.286c0-.473.398-.857.889-.857zM13.889 11h6.222a.9.9 0 0 1 .889.91v8.18a.9.9 0 0 1-.889.91H13.89a.9.9 0 0 1-.889-.91v-8.18a.9.9 0 0 1 .889-.91zM13.889 3h6.222c.491 0 .889.384.889.857v4.286c0 .473-.398.857-.889.857H13.89C13.398 9 13 8.616 13 8.143V3.857c0-.473.398-.857.889-.857z"/>
-                            </svg>
-                            Dashboard
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <div class="flex-grow container mx-auto sm:px-4 pt-6">
         <div class="bg-white border-t border-b shadow mb-6">
